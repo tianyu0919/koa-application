26bc61d5daccc9c1617865a4dd2d1331c9c417ce
卢天宇 <1141049271@qq.com>
2023-09-13 23:05:24 +0800

    feat: 新增koa-bodyparser插件用来解析post发送的数据

--- app.js
+++ app.js
@@ -5,14 +5,18 @@
  */
 const Koa = require('koa');
 const Router = require('@koa/router');
+const koaBodyparser = require('koa-bodyparser');
 const { fileLogger, consoleLogger } = require('./logger');
 
 const app = new Koa();
 const router = new Router();
 
+app.use(koaBodyparser());
+
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get('X-Response-Time');
+  ctx.response.set('access-control-allow-origin', '*');
   fileLogger.info(`${ctx.method} [${ctx.url}] - ${rt}`);
   consoleLogger.info(`${ctx.method} [${ctx.url}] - ${rt}`);
 });
@@ -32,6 +36,23 @@ router.get('/', async (ctx) => {
 router.get('/api', async (ctx) => {
   ctx.body = 'this api page';
 })
+
+router.post('/api', async (ctx) => {
+  const data = {
+    name: 'lucy',
+    age: 18
+  }
+  console.log(ctx.request.body);
+  await delay(2000);
+  ctx.body = JSON.stringify(data);
+})
+
+// 定义一个延迟函数
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 app.use(router.routes());
 app.listen(3000);
